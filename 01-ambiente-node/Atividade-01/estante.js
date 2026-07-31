9017a6ab8ee40621d9417f08be4227fe262dd4d5
Laura Nunes <laura.semensato@edu.senai.br>
2026-07-31 15:24:38 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/Atividade-01/estante.js
@@ -0,0 +1,16 @@
+// ===== LIVRO 1 =====
+const livro1 = {
+titulo: "Clean Code",
+autor: "Robert C. Martin",
+preco: 89.9,
+estoque: 12,
+};
+// ===== LIVRO 2 — TROQUE os quatro valores abaixo =====
+const livro2 = {
+titulo: "Nem te conto",
+autor: "Emilly Henry",
+preco: 65,
+estoque: 1,
+};
+// Esta linha entrega os dois livros para os outros arquivos usarem
+module.exports = { livro1, livro2 };
